1a4321b8caa5b13c90626f5606377117d7d18aff
NS3131 <shaumba@hotmail.com>
2025-07-25 17:31:03 +0100

    createfunctionjs

---
+++ script.js
@@ -0,0 +1,10 @@
+// let's first create a function called submitAnswers with
+// two variables 
+
+function submitAnswers(){
+    var totalQuestions = 3;
+    var correctAnswers = 0;
+}
+
+// let's get user input
+
